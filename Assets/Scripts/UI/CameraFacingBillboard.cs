a5834ff1761c6ebca6d3a239128fd5953d8973fd
sevenish7 <evans.samuel7@gmail.com>
2019-12-15 09:25:38 +1030

    forgot the actual completion bar script

---
+++ Assets/Scripts/UI/CameraFacingBillboard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class CameraFacingBillboard : MonoBehaviour
+{
+
+    private Camera cam;
+    private Canvas canvas;
+
+    private void Awake()
+    {
+        canvas = GetComponent<Canvas>();
+        cam = Camera.main;
+    }
+
+    private void FaceCamera()
+    {
+        Vector3 cameraVector = Vector3.zero;// = transform.position - cam.transform.position;
+        // cameraVector.x = transform.position.x - cam.transform.position.x;
+        cameraVector.y = transform.position.y - cam.transform.position.y;
+        cameraVector.z = transform.position.z - cam.transform.position.z;
+
+        var invCamDist = 1f / (float)System.Math.Sqrt(cameraVector.x * cameraVector.x + cameraVector.y * cameraVector.y + cameraVector.z * cameraVector.z);
+
+        cameraVector *= invCamDist;
+
+        transform.rotation = Quaternion.LookRotation(cameraVector);
+    }
+
+    private void LateUpdate()
+    {
+        FaceCamera();
+    }
+}
---
+++ Assets/Scripts/UI/CompletionBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompletionBar : SerializedMonoBehaviour
+{
+    [SerializeField] private Slider bar;
+    [SerializeField] private ICompleteable target;
+
+    public void HideShow(bool show)
+    {
+        gameObject.SetActive(show);
+    }
+
+    private void Update()
+    {
+        bar.value = target.GetPercentComplete();
+    }
+}
+
+public interface ICompleteable
+{
+    float GetPercentComplete();
+}
